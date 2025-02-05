1e74a8f706958d9a8ae74674858c86612126c854
pavarasan-karunainathan-ics4u1-02-25 <166336911+pavarasan-karunainathan-ics4u1-02-25@users.noreply.github.com>
2025-02-05 15:09:49 +0000

    Completed Problems

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,6 +1,8 @@
-public class PracticeProblem {
+import java.util.Scanner;
 
-	public static void main(String args[]) {
+public class PracticeProblem{
+	public static Scanner sc = new Scanner(System.in);
+	public static void main(String args[]){
 		q1();
 		q2();
 		q3();
@@ -9,34 +11,49 @@ public class PracticeProblem {
 		q6();
 	}
 
-	public static void q1() {
-		//Write question 1 code here
-		
+	public static void q1(){
+		sc = new Scanner(System.in);
+		System.setIn(System.in);
+		System.out.print("In: ");
+		System.out.println(sc.nextLine());
 	}
 
-	public static void q2() {
-		//Write question 2 code here
-		
+	public static void q2(){
+		sc = new Scanner(System.in);
+		System.out.print("In: ");
+		int num1 = sc.nextInt();
+		sc.nextLine();
+		System.out.print("In: ");
+		int num2 = sc.nextInt();
+		sc.nextLine();
+		System.out.println(num1/num2);
+		System.out.println(num1%num2);
 	}
 
-	public static void q3() {
-		//Write question 3 code here
-		
+	public static void q3(){
+		sc = new Scanner(System.in);
+		System.out.print("In: ");
+		System.out.println(sc.nextLine() + " was the text you wrote");
 	}
 
-	public static void q4() {
-		//Write question 4 code here
-		
+	public static void q4(){
+		sc = new Scanner(System.in);
+		System.out.print("In: ");
+		System.out.println(sc.nextInt() * 5);
+		sc.nextLine();
 	}
 
-	public static void q5() {
-		//Write question 5 code here
-		
+	public static void q5(){
+		sc = new Scanner(System.in);
+		System.out.print("In: ");
+		System.out.println(sc.nextBoolean() + " is a boolean");
+		if(sc.hasNextLine()) sc.nextLine();
 	}
 
-	public static void q6() {
-		//Write question 6 code here
-		
+	public static void q6(){
+		sc = new Scanner(System.in);
+		System.out.print("In: ");
+		System.out.println(sc.nextDouble() - 3.2);
+		if(sc.hasNextLine()) sc.nextLine();
 	}
-
 }
